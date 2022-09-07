ebd3407ffb163216354b2a00ee9a5503f200ffd1
Ren0503 <illuminati0440@gmail.com>
2022-09-07 22:47:42 +0700

    Init user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { NotificationsModule } from './notifications/notifications.module';
-import { VotesModule } from './votes/votes.module';
+import { PostsModule } from './posts/posts.module';
 import { TopicsModule } from './topics/topics.module';
+import { UsersModule } from './users/users.module';
+import { VotesModule } from './votes/votes.module';
 
 @Module({
-  imports: [UsersModule, PostsModule, CommentsModule, NotificationsModule, VotesModule, TopicsModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    UsersModule, 
+    PostsModule, 
+    CommentsModule, 
+    NotificationsModule, 
+    VotesModule, 
+    TopicsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.schema.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
+
+export type UserDocument = User & Document
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  birthDate: Date;
+
+  @Prop()
+  gender: boolean; // 0: female; 1: male
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  bio: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  avatar: string;
+
+  @Prop()
+  background: string;
+
+  @Prop()
+  role: Types.ObjectId;
+
+  @Prop()
+  followers: Types.ObjectId[];
+
+  @Prop()
+  followings: Types.ObjectId[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
